d6f6c40f18f1c4500f6820cfe7c0629042b45a7e
Sasa Gvozdenovic <sasa.gvozdenovic@shareit.rs>
2020-03-13 15:59:33 +0100

    First changes

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,48 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Border Cameras</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Border <a href="https://nextjs.org">Cameras!</a>
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        <img width="100px" src="/favicon.ico" />
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+        <a href="#" className="card">
+          <h3>Bosna</h3>
+          <p>&rarr; Rača</p>
+          <p>&rarr; Gunja</p>
+          <p>&rarr; Pavlović</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <a href="#" className="card">
+          <h3>Srbija &rarr;</h3>
+          <p>&rarr; Sremska Rača</p>
+          <p>&rarr; Batrovci</p>
+          <p>&rarr; Pavlović</p>
         </a>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
+        <a href="#" className="card">
+          <h3>Hrvatska &rarr;</h3>
+          <p>&rarr; Gunja</p>
+          <p>&rarr; Bajakovo</p>
+          <p>&rarr; Ilok</p>
         </a>
       </div>
     </main>
 
     <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        Dev
       </a>
     </footer>
 
@@ -68,7 +57,7 @@ const Home = () => (
       }
 
       main {
-        padding: 5rem 0;
+        padding: 2rem 0;
         flex: 1;
         display: flex;
         flex-direction: column;
@@ -148,6 +137,8 @@ const Home = () => (
 
       .card {
         margin: 1rem;
+        width: fill-available;
+        width: -webkit-fill-available;
         flex-basis: 45%;
         padding: 1.5rem;
         text-align: left;
@@ -198,6 +189,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
